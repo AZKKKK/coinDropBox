c14a5e828caa38435285f027eb342805ac83aa5f
AZKzzz4 <87250294+AZKKKK@users.noreply.github.com>
2023-09-22 22:56:43 -0700

    Add files via upload

---
+++ src/main/java/com/example/coindropbox/TextModel.java
@@ -0,0 +1,15 @@
+package com.example.coindropbox;
+
+public class TextModel {
+
+
+    public static void main(String[] args) {
+        BoxModel b = new BoxModel();
+        b.printPic();
+
+    }
+
+
+
+
+}
